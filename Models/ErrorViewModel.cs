05fc3fd530439aa5c7cd937de8e52a727dcca451
Caroline Tyler <cttyler16@gmail.com>
2018-03-12 14:07:28 -0600

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Carolines_Webpage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
