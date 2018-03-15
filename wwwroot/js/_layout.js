d239a6564e3883d99dec2e7a8c30a55221e2c72b
Caroline Tyler <cttyler16@gmail.com>
2018-03-14 20:10:20 -0600

    Pictures and jquery and other things

---
+++ wwwroot/js/_layout.js
@@ -0,0 +1,27 @@
+﻿function responsiveTabs() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+$(document).ready(function () {
+    if(window.location.href.indexOf('Home/About') != -1)
+    {
+         $("#navbar a#about").addClass("active");
+        return;
+    }
+    else if(window.location.href.indexOf('Home/Resume') != -1)
+    {
+         $("#navbar a#resume").addClass("active");
+        return;
+    } 
+    else if(window.location.href.indexOf('Home/Contact') != -1)
+    {
+         $("#navbar a#contact").addClass("active");
+        return;
+    } 
+    $("#navbar a#home").addClass("active");
+});
