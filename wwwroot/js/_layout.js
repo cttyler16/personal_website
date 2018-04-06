2bd4082e0ea168e5b9d1e5a58e33fa36db5c5928
Caroline Tyler <cttyler16@gmail.com>
2018-04-05 19:22:46 -0600

    Saving Change

--- wwwroot/js/_layout.js
+++ wwwroot/js/_layout.js
@@ -8,20 +8,27 @@
 }
 
 $(document).ready(function () {
-    if(window.location.href.indexOf('Home/About') != -1)
-    {
+    if(window.location.href.indexOf('Home/About') != -1){
          $("#navbar a#about").addClass("active");
         return;
     }
-    else if(window.location.href.indexOf('Home/Resume') != -1)
-    {
+    else if(window.location.href.indexOf('Home/Resume') != -1){
          $("#navbar a#resume").addClass("active");
         return;
     } 
-    else if(window.location.href.indexOf('Home/Contact') != -1)
-    {
+    else if(window.location.href.indexOf('Home/Contact') != -1){
          $("#navbar a#contact").addClass("active");
         return;
     } 
     $("#navbar a#home").addClass("active");
+});
+
+var positionSmall = 0;
+$(document).scroll(function () {
+    positionSmall = $(this).scrollTop();
+    if (positionSmall > 100) {
+        $(".navbar").css('background-color', '#8FC1E3');
+    } else {
+        $(".navbar").css('background-color', '');
+    }
 });
